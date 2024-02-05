164a9a7fe88693a5bef75d12423a664ec31bc254
Annaabb <Itsnotannaa20@gmail.com>
2024-02-05 20:20:14 +0200

    HW01_JS

---
+++ HW01_JS/main.js
@@ -0,0 +1,10 @@
+let userNickname = "Anna";
+let userscore = "20";
+
+let price = prompt('Вкажіть ціну покупки');
+let quantity = prompt ('Вкажіть кількість продуктів');
+let sum = price * quantity
+console.log = ('total amount is', sum);
+
+let discount = sum * 0.05;
+console.log('your discount is', discount)
